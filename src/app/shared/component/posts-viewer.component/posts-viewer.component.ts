4e8554868b28fdf08877b9aeda8822816c4b2eac
luanapaesok <luanapaes.ok@gmail.com>
2025-09-23 09:18:53 -0300

    feat: implementa função delete que recebe o id do usuário, apaga do banco e do array de exibição

--- src/app/shared/component/posts-viewer.component/posts-viewer.component.ts
+++ src/app/shared/component/posts-viewer.component/posts-viewer.component.ts
@@ -10,6 +10,8 @@ import {
 import { Post } from '../../interfaces/post';
 import { UsuariosService } from '../../services/usuarios.service';
 import { CreatePostDialog } from '../create-post-dialog/create-post-dialog';
+import { CustomPost } from '../../interfaces/custom-post.interface';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-posts-viewer.component',
@@ -17,31 +19,48 @@ import { CreatePostDialog } from '../create-post-dialog/create-post-dialog';
   templateUrl: './posts-viewer.component.html',
   styleUrl: './posts-viewer.component.scss'
 })
-export class PostsViewerComponent implements OnInit{
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {id: number, nome: string}) { }
+export class PostsViewerComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { id: number, nome: string }) { }
   readonly dialog = inject(MatDialog);
   usuariosService = inject(UsuariosService);
+  postService = inject(PostService)
   postsList: Post[] = []
 
   ngOnInit(): void {
     this.getPosts();
   }
 
-  getPosts(){
+  getPosts() {
     return this.usuariosService.getPostsUserByID(this.data.id).subscribe((user) => {
       user.posts.map((post) => this.postsList.push(post))
     })
   }
 
-  openDialogCreatePost(idUser: number){
+  openDialogCreatePost(idUser: number) {
     const dialogRef = this.dialog.open(CreatePostDialog, {
-      data: {idUser}
+      data: { idUser }
     });
 
     dialogRef.afterClosed().subscribe((novoPost) => {
-      if(novoPost){
+      if (novoPost) {
         this.postsList.push(novoPost);
       }
     })
+  }
+
+  delete(id: number) {
+    // apaga do banco
+    this.postService.delete(id).subscribe({
+      next: () => {
+        // apaga da usuariosList
+        const indice = this.postsList.findIndex(post => post.id === id);
+
+        //quando retorna -1 é porque não encontrou o obj pelo id
+        if (indice > -1) {
+          this.postsList.splice(indice, 1);
+        }
+      },
+      error: (e) => console.log("Post não encontrado.")
+    });
   }
 }
