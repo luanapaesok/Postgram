af353a5b1b95db5465c52a322bddcc75485db39d
luanapaesok <luanapaes.ok@gmail.com>
2025-09-25 11:05:07 -0300

    feat: add LoginService com funções de login e verificações de token

---
+++ src/app/shared/services/login.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { Observable, tap } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  readonly urlAPI = "http://localhost:3000/auth/login";
+  httpClient = inject(HttpClient);
+  router = inject(Router)
+
+  isBrowser(): boolean {
+    return typeof window !== 'undefined' && typeof localStorage !== 'undefined';
+  }
+
+  login(email: string, password: string): Observable<any> {
+    return this.httpClient.post<any>(this.urlAPI, { email, password }).pipe(
+      tap((resposta) => {
+        // Verifica se o token foi recebido
+        if (!resposta.accessToken || !this.isBrowser()) return;
+
+        // Armazenando o token no localStorage
+        localStorage.setItem('token', btoa(JSON.stringify(resposta.accessToken)));
+      })
+    );
+  }
+
+  logado() {
+    return this.isBrowser() && localStorage.getItem('token') ? true : false;
+  }
+}
