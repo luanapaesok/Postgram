ef4bad7f1bdab293c6df2cf88bb3db073d10b7aa
luanapaesok <luanapaes.ok@gmail.com>
2025-09-25 11:15:46 -0300

    feat: caso o usuário não esteja logado ele é redirecionado para a tela de login, caso esteja, o acesso à rota `home` é permitido.

--- src/app/guards/auth.guards.ts
+++ src/app/guards/auth.guards.ts
@@ -1,14 +1,20 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { LoginService } from "../shared/services/login.service";
+import { Router } from "@angular/router";
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard {
-
+  router = inject(Router)
   constructor(
     private authService: LoginService
   ) { }
 
   canActivate() {
-    return this.authService.logado();
+    if(this.authService.logado()){
+      return true
+    } else {
+      this.router.navigate(['home']);
+      return false
+    }
   }
 }
