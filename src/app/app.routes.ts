4d05167a9696137fcd37960b1860f264cc17bb2e
luanapaesok <luanapaes.ok@gmail.com>
2025-09-25 11:06:41 -0300

    feat: adiciona AuthGuard à rota `home`

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home-component/home-component';
+import { Login } from './pages/login/login';
+import { AuthGuard } from './guards/auth.guards';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: '', 
+        component: Login,
+        title: 'Faça Login'
+    },
+    {
+        path: 'home',
         component: HomeComponent,
-        title: "Página Inicial"
-    }
+        title: "Página Inicial", 
+        canActivate: [AuthGuard]
+    },
 ];
